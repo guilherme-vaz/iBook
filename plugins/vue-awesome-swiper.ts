ecd3f8971d3c8633e5f88b3aeb2819b05ccd2f3d
Guilherme Vaz <guilhermeolivaaz@gmail.com>
2021-11-22 16:31:52 -0300

    Added error layout and reading page, both not finished, also deleting vue-awesome-swiper plugin and using just swiper instead

--- plugins/vue-awesome-swiper.ts
+++
@@ -1,9 +0,0 @@
-// plugins/vue-awesome-swiper.js
-
-import Vue from 'vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-
-
-import 'swiper/swiper-bundle.css'
-
-Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
